5927f933fd52b4891ae9651fd38076cfadccec78
Jason Sanford <jasonsanford@gmail.com>
2014-07-25 08:51:25 -0400

    Put a bird on it.

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -5,7 +5,15 @@ var map = new L.mapbox.Map('map-container', 'jcsanford.j25ef8lg');
 L.control.locate().addTo(map);
 
 var geojson_layer_options = {
-  pointToLayer: L.mapbox.marker.style
+  pointToLayer: L.mapbox.marker.style,
+  onEachFeature: function (feature, layer) {
+    var html = '<h3>' + feature.properties.title + '</h3>';
+    if (feature.properties.description) {
+      html += '<p>' + feature.properties.description + '</p>';
+    }
+    html += '<div class="put"></div>';
+    layer.bindPopup(html);
+  }
 };
 var geojson_layer = new L.GeoJSON(null, geojson_layer_options);
 geojson_layer.addTo(map);
